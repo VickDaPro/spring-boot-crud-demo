4e81135b2c0b90bc03f07cf4f35b5d8db0bff14f
VickDaPro <vickbishnoi@gmail.com>
2025-07-13 19:59:49 +0530

    finalised the project with very basic crud functionality with mysql db and entityManager

--- src/main/java/com/vickpro/springboot_crud_demo/dao/StudentDAO.java
+++ src/main/java/com/vickpro/springboot_crud_demo/dao/StudentDAO.java
@@ -2,9 +2,21 @@ package com.vickpro.springboot_crud_demo.dao;
 
 import com.vickpro.springboot_crud_demo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByFirstName(String theFirstName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    int deleteAll();
 }
--- src/main/java/com/vickpro/springboot_crud_demo/dao/StudentDAOImpl.java
+++ src/main/java/com/vickpro/springboot_crud_demo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.vickpro.springboot_crud_demo.dao;
 
 import com.vickpro.springboot_crud_demo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -30,4 +33,50 @@ public class StudentDAOImpl implements StudentDAO {
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
     }
+
+    @Override
+    public List<Student> findAll() {
+        // create query
+//        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        // return query results
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByFirstName(String theFirstName) {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE firstName=:theData", Student.class);
+
+        // set query params
+        theQuery.setParameter("theData", theFirstName);
+
+        // return query results
+        return theQuery.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        // Retrieve the student
+        Student theStudent = entityManager.find(Student.class, id);
+
+        // delete the student
+        entityManager.remove(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+        int numRowsDeleted = entityManager.createQuery("DELETE FROM Student").executeUpdate();
+        return numRowsDeleted;
+    }
+
 }
